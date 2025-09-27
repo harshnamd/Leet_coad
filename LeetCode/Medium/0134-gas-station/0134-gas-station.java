285ff4b46f36ac40cdab77ccbe6ca7efa7e5e3d5
Harsh Namdev <163432385+harshnamd@users.noreply.github.com>
2025-09-27 09:57:56 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ LeetCode/Medium/0134-gas-station/0134-gas-station.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int totalgas = 0;
+        int totalcost = 0;
+
+        for (int i = 0; i < gas.length; i++) {
+            totalgas += gas[i];
+            totalcost += cost[i];
+        }
+
+        if (totalgas < totalcost) {
+            return -1;
+        }
+
+        int c = 0;
+        int s = 0;
+
+        for (int i = 0; i < gas.length; i++) {
+            c -= gas[i] - cost[i];
+
+            if (c < 0) {
+                s = i + 1;
+                c = 0;
+            }
+        }
+
+        return s;
+    }
+}
