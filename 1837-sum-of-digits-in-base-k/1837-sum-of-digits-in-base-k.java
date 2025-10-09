d83e8f55a45ae03f93a7d9b84e11eed43e164c82
Harsh Namdev <163432385+harshnamd@users.noreply.github.com>
2025-10-09 18:52:44 +0530

    Time: 0 ms (100%), Space: 40.4 MB (54.96%) - LeetHub

---
+++ 1837-sum-of-digits-in-base-k/1837-sum-of-digits-in-base-k.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int sumBase(int n, int k) {
+        if (n < k)
+            return n;
+        int ans = 0;
+        int div = 0;
+        while (n >= k) {
+            int rem = n % k;
+            ans += rem;
+            div = n / k;
+            n = div;
+        }
+        ans += div;
+        return ans;
+    }
+}
