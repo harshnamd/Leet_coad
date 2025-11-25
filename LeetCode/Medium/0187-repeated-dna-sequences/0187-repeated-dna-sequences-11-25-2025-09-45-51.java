bf7d9b06776788bff59f0e7d497fd5fd1cf0e59b
Harsh Namdev <163432385+harshnamd@users.noreply.github.com>
2025-11-25 09:45:52 +0530

    Time: 15 ms (93.86%), Space: 55.8 MB (10.47%) - LeetHub

---
+++ LeetCode/Medium/0187-repeated-dna-sequences/0187-repeated-dna-sequences-11-25-2025-09-45-51.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public List<String> findRepeatedDnaSequences(String s) {
+        List<String> res = new ArrayList<String>();
+        Set<String> resset = new HashSet<String>();
+        if(s == null || s.length() <= 10){
+            return res;
+        }
+        Set<String> set = new HashSet<String>();
+        int len = s.length();
+        for(int i = 0; i <= len - 10; i++){
+            String sub = s.substring(i, i + 10);
+            if(!set.add(sub)){
+                resset.add(sub);
+            }
+        }
+        res.addAll(resset);
+        return res;
+    }
+}
